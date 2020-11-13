69aeae047657d29529b3508a5cd0868be727a6a0
hieu <hieutcps09202@fpt.edu.vn>
2020-11-13 11:02:06 +0700

    them

---
+++ src/main/java/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package repository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.laptrinhjavaweb.entity.CategoryEntity;
+public interface CategoryRepository extends JpaRepository<CategoryEntity, Long>{
+	CategoryEntity findOneByCode(String code);
+}
---
+++ src/main/java/repository/NewRepository.java
@@ -0,0 +1,9 @@
+package repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.laptrinhjavaweb.entity.NewEntity;
+
+public interface NewRepository extends JpaRepository<NewEntity, Long>{
+	
+}
